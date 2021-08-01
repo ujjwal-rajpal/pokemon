5a8ab15c953b7ebe89f40507e7e5b036255ecfbf
Ujjwal Rajpal <ujjwalr@mindfiresolutions.com>
2021-08-01 13:04:47 +0530

    add new dependicies routing, angular flex and angular material

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MyMaterialModule } from './material.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    MyMaterialModule
+  ]
+})
+export class SharedModule { }
