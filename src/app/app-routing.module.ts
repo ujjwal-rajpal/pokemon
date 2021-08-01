f126e28573cde08291596abba15dff1b0ef4cccc
Ujjwal Rajpal <ujjwalr@mindfiresolutions.com>
2021-08-01 17:36:44 +0530

    add home module and routing to home module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import {NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Routes, RouterModule} from '@angular/router';
 
-
+import { HomeModule } from "./home/home.module"
 const routes :Routes = [
-    
+    { path: '', loadChildren: ()=> HomeModule }
 ]
 @NgModule({
     declarations: [],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from "./app-routing.module";
 
 import { HeaderComponent } from './include/header/header.component';
 import { FooterComponent } from './include/footer/footer.component';
@@ -19,7 +20,8 @@ import { FooterComponent } from './include/footer/footer.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
